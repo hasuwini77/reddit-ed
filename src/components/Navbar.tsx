a8d3eb6d49ee4df2523c7a3e19a24540c5a1c233
Edwin Andermyr <andermyredwin@gmail.com>
2024-10-10 16:20:21 +0200

    added a nice glowing effect on search bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ export default function MyNavbar() {
             <input
               type="search"
               placeholder="Search Reddit..."
-              className="w-full h-12 rounded-full bg-gray-700 text-white placeholder-gray-400 border border-gray-600 focus:border-purple-400 focus:ring focus:ring-purple-700 pl-10 pr-4 transition"
+              className="w-full h-12 rounded-full bg-gray-700 text-white placeholder-gray-400 border border-gray-200 focus:border-yellow-300 focus:ring focus:ring-yellow-100 focus:ring-opacity-10 focus:outline-none transition duration-300 ease-in-out pl-10 pr-4"
             />
             <SearchIcon
               className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
