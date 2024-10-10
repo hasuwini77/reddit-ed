29a7ce0caabb53832cf8e90abd224f1eb264c63e
Edwin Andermyr <andermyredwin@gmail.com>
2024-10-10 15:56:34 +0200

    added the aceternity sidebar

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["i.pravatar.cc"],
+    domains: ["i.pravatar.cc", "assets.aceternity.com"],
   },
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import MyNavbar from "@/components/Navbar";
+import { MySidebar } from "@/components/Sidebar";
+
 export default function Home() {
   return (
     <div className="font-sans">
       <MyNavbar />
+      <MySidebar />
     </div>
   );
 }
