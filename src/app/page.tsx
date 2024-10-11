ef455f7d2e4048dd4b33cda864c64169cded696b
Edwin Andermyr <andermyredwin@gmail.com>
2024-10-11 14:52:41 +0200

    trying to figure out the dashboard system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+
 import MyNavbar from "@/components/Navbar";
 import { MySidebar } from "@/components/Sidebar";
+import { useState } from "react";
 
 export default function Home() {
+  const [isCreatePostClicked, setIsCreatePostClicked] = useState(false);
+
+  const handleCreatePostClick = () => {
+    setIsCreatePostClicked(true);
+  };
+
   return (
     <div className="font-sans">
       <MyNavbar />
