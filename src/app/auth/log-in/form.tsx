253b01f2041e0839516bdf0ded85b8db1d84d6f7
Edwin Andermyr <andermyredwin@gmail.com>
2024-11-09 12:54:06 +0100

    added the invalid credential case to log in form

--- src/app/auth/log-in/form.tsx
+++ src/app/auth/log-in/form.tsx
@@ -14,35 +14,63 @@ import { Loader2 } from "lucide-react";
 export const LogInForm = () => {
   const { mutate, error, isPending } = useMutation({
     mutationFn: logIn,
+    onError: (error: any) => {
+      console.error("Login error:", error);
+    },
   });
 
   const {
     register,
     handleSubmit,
+    setError,
     formState: { errors },
   } = useForm<z.infer<typeof logInSchema>>({
     resolver: zodResolver(logInSchema),
     mode: "onBlur",
   });
 
+  const onSubmit = async (values: z.infer<typeof logInSchema>) => {
+    try {
+      await mutate(values);
+    } catch (error: any) {
+      if (error.message === "Email not registered") {
+        setError("email", {
+          type: "manual",
+          message: "This email is not registered",
+        });
+      } else if (error.message === "Invalid credentials") {
+        setError("password", {
+          type: "manual",
+          message: "Invalid email or password",
+        });
+      }
+    }
+  };
+
   return (
     <form
-      onSubmit={handleSubmit((values) => mutate(values))}
+      onSubmit={handleSubmit(onSubmit)}
       className="flex w-full max-w-md flex-col gap-4 text-white"
     >
-      <Input
-        {...register("email")}
-        error={errors.email}
-        placeholder="Your Email"
-        className="placeholder:text-white placeholder:opacity-50"
-      />
-      <Input
-        {...register("password")}
-        type="password"
-        placeholder="Password"
-        error={errors.password}
-        className="placeholder:text-white placeholder:opacity-50"
-      />
+      <div className="flex flex-col gap-1">
+        <Input
+          {...register("email")}
+          error={errors.email}
+          placeholder="Your Email"
+          className="placeholder:text-white placeholder:opacity-50"
+        />
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <Input
+          {...register("password")}
+          type="password"
+          placeholder="Password"
+          error={errors.password}
+          className="placeholder:text-white placeholder:opacity-50"
+        />
+      </div>
+
       <Button className="bg-slate-800" type="submit">
         {isPending ? (
           <>
@@ -52,6 +80,7 @@ export const LogInForm = () => {
           "Log in"
         )}
       </Button>
+
       {error && <p className="text-red-500 p-1">{error.message}</p>}
     </form>
   );
